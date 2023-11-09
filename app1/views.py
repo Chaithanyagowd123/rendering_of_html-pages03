c6ac26f611c2527306cffcc43f65efd770760526
Chaithanyagowd@1234 <kaithepallichaithanya@gmail.com>
2023-11-09 21:05:14 +0530

    first-commit

---
+++ app1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def chaithu(request):
+    return render(request,'webpage1.html')
+
+def bhanu(request):
+    return render(request,'webpage2.html')
+
+def krishna(request):
+    return render(request,'webpage3.html')
+
+def jagadeesh(request):
+    return render(request,'webpage4.html')
+
+# Create your views here.
